d2ee1e447bbcf792eccee6b4bc8ae166be77591a
Aakodal <neshelldev@gmail.com>
2021-02-07 18:23:26 +0100

    refactor(scripts): refactor firefox script

--- scripts/update-or-install-firefox-dev.sh
+++ scripts/update-or-install-firefox-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-firefox_install="/opt/firefox-dev/"
+FIREFOX_INSTALL="/opt/firefox-dev/"
 
 
 curl --progress-bar -Lo firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=fr"
@@ -9,11 +9,11 @@ tar -xjf firefox.tar.bz2
 
 rm -rf firefox.tar.bz2
 
-while [ ! -z "$(ps -aux | grep -v grep | grep "$firefox_install")" ]; do
+while [ ! -z "$(ps -aux | grep -v grep | grep "$FIREFOX_INSTALL")" ]; do
 	read -p "Firefox is running. Retry? "
 done
 
-cp -rf firefox/* $firefox_install || sudo cp -rf firefox/* $firefox_install
+cp -rf firefox/* $FIREFOX_INSTALL || sudo cp -rf firefox/* $FIREFOX_INSTALL
 
 rm -rf firefox/
 
